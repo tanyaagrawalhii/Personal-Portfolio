2d246892a735068ddcdd57bf21d0eb1cc5d44b7a
tanyaagrawalhii <144205418+tanyaagrawalhii@users.noreply.github.com>
2024-07-10 21:56:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ var tabcontents=document.getElementsByClassName("contents");
             
     }
 
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src="dark theme icon/sun.png";
+    }
+    else{
+        icon.src='dark theme icon/moon.png';
+    }
+}  
+
+
+    
